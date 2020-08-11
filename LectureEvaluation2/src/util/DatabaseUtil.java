0fe61cb5432d7eb5d792d30caf46595df9e2ab09
starflutet <starflutet@gmail.com>
2020-08-11 12:48:05 +0900

    sucsess

---
+++ LectureEvaluation2/src/util/DatabaseUtil.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseUtil {
+	
+	public static Connection getConnection() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/Lectureevaluation?useUnicode=true&characterEncoding=utf8";
+			String dbID = "root";
+			String dbPassword = "1234";
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection(dbURL,dbID,dbPassword);			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+}
---
+++ LectureEvaluation2/src/util/Gmail.java
@@ -0,0 +1,11 @@
+package util;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class Gmail extends Authenticator {
+    @Override
+    protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication("starflutet1@gmail.com","@jhp21331993@");
+    }
+}
+
